e07663653c8d7c5127021fa96662b6ca35717a8c
sravanitammali <147266490+sravanitammali@users.noreply.github.com>
2025-01-03 14:57:17 +0530

    Create color4.hs

---
+++ color4.hs
@@ -0,0 +1,6 @@
+data Color = Red | Green | Blue | White
+describeColor :: Color -> String
+describeColor Red    = "This is Red"
+describeColor Green  = "This is Green"
+describeColor Blue   = "This is Blue"
+describeColor White   = "This is White"
